532522c3c8a50aae1968d5084a5680c96ea698a9
Rodrigaum <rodrigodejesus2208@gmail.com>
2022-10-08 20:09:00 -0300

    Update main to fit the branch requirements

--- main.py
+++ main.py
@@ -1,42 +1,64 @@
-import inspect
-from . import Caesar, Utils
+from string import ascii_lowercase
+
+ALPHABET = list(ascii_lowercase)
+SHIFT = 389012391290831902309125890123089190
+OPTIONS = (
+    (0, "encrypt", lambda msg, shift: caesar_algorithm(msg, shift, 1)),
+    (1, "decrypt", lambda msg, shift: caesar_algorithm(msg, shift, 0)),
+)
+
+
+def choose_option(options_list):
+    for option_element in options_list:
+        print(f"{option_element[0]}: {option_element[1]}\n")
+    
+    while True:
+        try:
+            user_option = int(input("Choose an option: "))
+
+            option = None
+            for menu_option in options_list:
+                if menu_option[0] == user_option:
+                    option = tuple(menu_option)
+
+            if option is None:
+                raise ValueError()
+            else:
+                return option
+        except ValueError:
+            print("Invalid option, try again")
+
+
+def caesar_algorithm(msg, shift, operation_code):
+    final_msg = ""
+    for char in msg:
+        try:
+            # ALPHABET.index(char.lower()) raises a ValueError if there is no such item in the list.
+            new_index = ALPHABET.index(char.lower())
+            if operation_code:
+                new_index += shift
+            else:
+                new_index -= shift
+
+            new_index %= len(ALPHABET)
+            final_msg += ALPHABET[new_index].upper() if char.isupper() else ALPHABET[new_index]
+        except ValueError: 
+            final_msg += char
+
+    return final_msg
 
 
 def main():
-    should_terminate = False
-    caesar = Caesar(0)
-    while not should_terminate:
-            action_option = Utils.choose_option(caesar.OPTIONS)
-
-            if action_option and len(action_option) > 2:
-                action_parameters_list = [parameter for parameter in inspect.signature(action_option[2]).parameters]
-                if "msg" in action_parameters_list:
-                    msg_option = Utils.choose_option(caesar.MSG_OPTIONS)
-
-                    if msg_option:
-                        user_msg = msg_option[2]()
-
-                        if "shift" in action_parameters_list:
-                            shift = int(input("What's the shift? "))
-                            result = action_option[2](user_msg, shift)
-                        else:
-                            result = action_option[2](user_msg)
-
-                        print("\n-------Result-------")
-                        if inspect.isgenerator(result):
-                            for i in result:
-                                print(i)
-                        else:
-                            print(result)
-                        print("--------------------")
-                else:
-                    alphabet_option = Utils.choose_option(caesar.ALPHABET_OPTIONS)
-                    caesar.change_alphabet(alphabet_option[0])
-                    print("Changed alphabet successfully")
-                    
-            elif action_option:
-                should_terminate = True
+    option = choose_option(OPTIONS)
+    if len(option) > 2:
+        user_msg = input("Type your message:\n")
+        while not user_msg:
+            print("Empty message. Try again:\n")
         
+        print(option[2](user_msg, SHIFT))
+
+            
+
 
 if __name__ == "__main__":
     main()
